673afb96af26d8ee4763facd01dea95e55542e47
DaJunnn <z1155340@gmail.com>
2026-07-02 18:19:40 +0800

    0702實作與範例

---
+++ 0702/ProfileOutput.java
@@ -0,0 +1,15 @@
+public class ProfileOutput {
+    public static void main(String[] args) {
+        String name = "Dajun";
+        int age = 22;
+        String department = "Information Management";
+        String course = "Programming";
+
+        System.out.println("=== Profile ===");
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Department: " + department);
+        System.out.println("Favorite Course: " + course);
+    }
+}
---
+++ 0702/UserInfo.java
@@ -0,0 +1,13 @@
+public class UserInfo {
+    public static void main(String[] args) {
+        String userName = "Dajun";
+        int userId = 411631178;
+        String department = "nformation Management";
+        int expectedScore = 90;
+
+         System.out.println("Name: " + userName);
+         System.out.println("ID: " + userId);
+         System.out.println("Department: " + department);
+         System.out.println("expectedScore: " + expectedScore);
+    }
+}
